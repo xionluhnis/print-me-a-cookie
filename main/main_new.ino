55cca43d9c393ba1b464f9d79944df661e8ca505
Alexandre Kaspar <akaspar@mit.edu>
2015-07-16 13:18:03 -0400

    New step system and stepBy instead of moveBy, and extrusion

--- main/main_new.ino
+++ main/main_new.ino
@@ -19,19 +19,41 @@ Example based off of demos by Brian Schmalz (designer of the Big Easy Driver).
 http://www.schmalzhaus.com/EasyDriver/Examples/EasyDriverExamples.html
 ******************************************************************************/
 //Declare pin functions on Arduino
-#define stp 2
-#define dir 3
-#define MS1 4
-#define MS2 5
-#define MS3 6
-#define EN  7
-
-#define stpY 8
-#define dirY 9
-#define MS1Y 10
-#define MS2Y 11
-#define MS3Y 12
-#define ENY  13
+// EXTRUDER 0
+#define STP_E0  2
+#define DIR_E0  3
+#define MS1_E0  4
+#define MS2_E0  5
+#define MS3_E0  6
+#define EN_E0   7
+// EXTRUDER 1
+#define STP_E1  -1
+#define DIR_E1  -1
+#define MS1_E1  -1
+#define MS2_E1  -1
+#define MS3_E1  -1
+#define EN_E1   -1
+// X AXIS STEPPER
+#define STP_X   -1
+#define DIR_X   -1
+#define MS1_X   -1
+#define MS2_X   -1
+#define MS3_X   -1
+#define EN_X    -1
+// Y AXIS STEPPER
+#define STP_Y   8
+#define DIR_Y   9
+#define MS1_Y   10
+#define MS2_Y   11
+#define MS3_Y   12
+#define EN_Y    13
+// Z AXIS STEPPER
+#define STP_Z   -1
+#define DIR_Z   -1
+#define MS1_Z   -1
+#define MS2_Z   -1
+#define MS3_Z   -1
+#define EN_Z    -1
 
 // errors
 #define ERR_NONE  0
@@ -41,61 +63,89 @@ int error;
 // delays in milliseconds
 #define DELAY_INTER 50
 #define DELAY_AFTER 30
+#define SECOND      (1000L/80L)
+
+// group sizes
+#define NUM_PINS 54
+#define NUM_OUTS (6 * 2)
+#define NUM_ENS  5
+// pin groups
+int enPins[NUM_ENS];
+int ouPins[NUM_OUTS];
 
 // state data
-
-#define NUM_PINS 54
-
 struct PinProcess {
-  unsigned long total; // total number of iterations
+  unsigned long moves; // total number of action triggers
   unsigned long steps; // number of steps to trigger action
-  unsigned long count; // accumulation buffer
+  unsigned long count; // step accumulation buffer
   int first;
   int second;
+  int eid; // enable pin id
 };
 
 struct PinProcess processes[NUM_PINS];
 
 byte isPending(int pin) {
-  return processes[pin].total > 0;
+  return processes[pin].moves > 0;
+}
+byte isEnabler(int pin) {
+  for(int i = 0; i < NUM_ENS; ++i)
+    if(enPins[i] == pin)
+      return 1;
+  return 0;
 }
 
-//Declare variables for functions
-char user_input;
-int x;
-int y;
-int state;
+// declarations (not needed for Arduino)
+void stepBy(int stpPin, int dirPin, long move, unsigned long speed, unsigned long init){
 
 //Reset Big Easy Driver pins to default states
 void resetBEDPins() {
-  digitalWrite(stp, LOW);
-  digitalWrite(dir, LOW);
-  digitalWrite(MS1, LOW);
-  digitalWrite(MS2, LOW);
-  digitalWrite(MS3, LOW);
-  digitalWrite(EN, HIGH);
-  digitalWrite(stpY, LOW);
-  digitalWrite(dirY, LOW);
-  digitalWrite(MS1Y, LOW);
-  digitalWrite(MS2Y, LOW);
-  digitalWrite(MS3Y, LOW);
-  digitalWrite(ENY, HIGH);
+  digitalWrite(STP_E0, LOW);
+  digitalWrite(DIR_E0, LOW);
+  digitalWrite(MS1_E0, LOW);
+  digitalWrite(MS2_E0, LOW);
+  digitalWrite(MS3_E0, LOW);
+  digitalWrite(EN_E0, HIGH);
+  digitalWrite(STP_Y, LOW);
+  digitalWrite(DIR_Y, LOW);
+  digitalWrite(MS1_Y, LOW);
+  digitalWrite(MS2_Y, LOW);
+  digitalWrite(MS3_Y, LOW);
+  digitalWrite(EN_Y, HIGH);
 }
 
 void setup() {
-  error = ERR_NONE;
-  pinMode(stp, OUTPUT);
-  pinMode(dir, OUTPUT);
-  pinMode(MS1, OUTPUT);
-  pinMode(MS2, OUTPUT);
-  pinMode(MS3, OUTPUT);
-  pinMode(EN, OUTPUT);
-  pinMode(stpY, OUTPUT);
-  pinMode(dirY, OUTPUT);
-  pinMode(MS1Y, OUTPUT);
-  pinMode(MS2Y, OUTPUT);
-  pinMode(MS3Y, OUTPUT);
-  pinMode(ENY, OUTPUT);
+  // pin groups
+  int enpins[] = { EN_E0, EN_E1, EN_X, EN_Y, EN_Z };
+  for(int i = 0; i < NUM_EN; ++i) enPins[i] = enpins[i];
+  int oupins[] = { STP_E0, DIR_E0, MS1_E0, MS2_E0, MS3_E0, EN_E0,
+                   STP_Y,  DIR_Y,  MS1_Y,  MS2_Y,  MS3_Y,  EN_Y };
+  for(int i = 0; i < NUM_OUTS; ++i) ouPins[i] = oupins[i];
+  // processes setups
+  for(int i = 0; i < NUM_PINS; ++i){
+    processes[i].moves = 0;
+    processes[i].steps = 1;
+    processes[i].count = 0;
+    processes[i].first = HIGH;
+    processes[i].second = LOW;
+    processes[i].eid = 0;
+  }
+  // set eid of processes
+  int eid = 0;
+  for(int i = NUM_OUTS - 1; i >= 0; --i){
+    if(isEnabler(i))
+      eid = i;
+    processes[i].eid = eid;
+  }
+  // set enabler LOW-HIGH instead of HIGH-LOW
+  for(int i = 0; i < 5; ++i){
+    processes[enPins[i]].first = LOW;
+    processes[enPins[i]].second = HIGH;
+  }
+  // set outputs
+  for(int i = 0; i < 6 * 2; ++i){
+    pinMode(ouPins[i], OUTPUT);
+  }
   resetBEDPins(); //Set step, direction, microstep and enable pins to default states
   Serial.begin(9600); //Open Serial connection for debugging
 }
@@ -125,9 +175,9 @@ byte process() {
         digitalWrite(i, processes[i].second);
       }
       // decrement remaining time
-      processes[i].total--;
       processes[i].count--;
-      if(processes[i].total > processes[i].steps){
+
+      if(processes[i].moves > 0){
         state = STATE_BUSY; // we are busy with one process at least
       }
     }
@@ -155,7 +205,7 @@ int readInt(){
   return sign * val;
 }
 
-unsigned long readLong(){
+unsigned long readULong(){
   unsigned long val = 0L;
   while(Serial.available()){
     char c = Serial.read();
@@ -173,6 +223,36 @@ unsigned long readLong(){
   return val;
 }
 
+long readLong(){
+  long val = 0L;
+  long sign = 1L;
+  while(Serial.available()){
+    char c = Serial.read();
+    if(c == ' '){
+      break;
+    }
+    long d = (long)(c - '0');
+    if(val == 0L && c == '-'){
+      sign = -1L;
+    } else if(d < 0 || d > 9){
+      error = ERR_PARSE;
+      return val;
+    } else {
+      val *= 10L;
+      val += d;
+    }
+  }
+  return sign * val;
+}
+
+void resetAll(){
+  for(int i = 0; i < NUM_PINS; ++i){
+    processes[i].moves = 0;
+  }
+  error = ERR_NONE; // clean flag
+  Serial.println("Reset.");
+}
+
 // process user input
 void readCommands(){
   if(Serial.available()){
@@ -183,13 +263,13 @@ void readCommands(){
       case 'p': {
         while(Serial.available()){
           int pin = readInt();
-          unsigned long total = readLong();
+          unsigned long moves = readLong();
           unsigned long steps = readLong();
           unsigned long count = readLong();
           if(steps == 0){
             steps = 1;
           }
-          processes[pin].total = total;
+          processes[pin].moves = moves;
           processes[pin].steps = steps;
           processes[pin].count = count;
         }
@@ -197,30 +277,36 @@ void readCommands(){
       // reset all processes (needed in case of error)
       case 'r':
       case 'R': {
-        for(int i = 0; i < NUM_PINS; ++i){
-          processes[i].total = 0;
-        }
-        error = ERR_NONE; // clean flag
-        Serial.println("Reset.");
+        resetAll();
       } break;
 
       // moveby dx dy dz sx [sy sz ix iy iz]
       case 'm': {
-        int dx = readInt(),
-            dy = readInt(),
-            dz = readInt();
-        unsigned long sx = readLong(),
-                      sy = readLong(),
-                      sz = readLong(),
-                      ix = readLong(),
-                      iy = readLong(),
-                      iz = readLong();
+        long dx = readLong(),
+             dy = readLong(),
+             dz = readLong();
+        unsigned long sx = readULong(),
+                      sy = readULong(),
+                      sz = readULong(),
+                      ix = readULong(),
+                      iy = readULong(),
+                      iz = readULong();
         if(sx == 0) sx = 1;
         if(sy == 0) sy = sx;
         if(sz == 0) sz = sx;
-        // if(dx) moveBy(stpX, dirX, dx, sx, ix);
-        if(dy) moveBy(stpY, dirY, dy, sy, iy);
-        // if(dy) moveBy(stpZ, dirZ, dz, sz, iz);
+        // if(dx) stepBy(STP_X, DIR_X, dx, sx, ix);
+        if(dy) stepBy(STP_Y, DIR_Y, dy, sy, iy);
+        // if(dy) stepBy(STP_Z, DIR_Z, dz, sz, iz);
+      } break;
+
+      // extrudeBy total steps init id
+      case 'e': {
+        long delta = readLong();
+        unsigned long speed = readULong(),
+                      init  = readULong();
+        int id = readInt();
+        if(id == 0) stepBy(STP_E0, DIR_E0, delta, speed, init);
+        // if(id == 1) stepBy(STP_E1, DIR_E1, delta, speed, init);
       } break;
 
       default:
@@ -246,6 +332,30 @@ void logError() {
   error = -1;
 }
 
+void begin() {
+  for(int i = 0; i < NUM_PINS; ++i){
+    int eid = processes[i].eid;
+    if(isPending(i) && eid > 0){
+      digitalWrite(eid, LOW);
+    }
+  }
+}
+void end() {
+  int active[NUM_PINS];
+  for(int i = 0; i < NUM_PINS; ++i){
+    active[i] = 0;
+    int eid = processes[i].eid;
+    if(isPending(i) && eid > 0){
+      active[i] = 1;
+    }
+  }
+  for(int i = 0; i < NUM_PINS; ++i){
+    if(isEnabler(i) && active[i] == 0){
+      digitalWrite(i, LOW);
+    }
+  }
+}
+
 // Main loop
 void loop() {
   // show errors
@@ -254,132 +364,86 @@ void loop() {
   // 1 = react to external events
   react();
 
-  // 1 = process scheduled events
+  // 2 = enable outputs that are not idle
+  begin();
+
+  // 3 = process scheduled events
   byte state = STATE_IDLE;
   if(error == ERR_NONE){
     state = process();
   }
-  // reset all states
-  resetBEDPins();
 
-  // 2 = read user input
+  // 3 = disable outputs that are idle
+  end();
+
+  // 4 = read user input
   if(state == STATE_IDLE){
     readCommands();
   }
 }
-void moveBy(int stpPin, int dirPin, int move, int speed, int init){
+void stepBy(int stpPin, int dirPin, long move, unsigned long speed, unsigned long init){
     
   // direction setting
-  processes[dirPin].total = 1;
+  processes[dirPin].moves = 1;
   processes[dirPin].steps = 1;
   processes[dirPin].count = 0;
-  processes[dirPin].first = processes[dirY].second = dy > 0 ? LOW : HIGH;
+  processes[dirPin].first = processes[dirPin].second = move > 0 ? LOW : HIGH;
   // stepper movements
-  processes[stpPin].total = move * speed;
+  processes[stpPin].moves = (unsigned long)(move > 0 ? move : -move) * speed;
   processes[stpPin].steps = speed;
-  processes[stpPin].count = init;
+  processes[stpPin].count = init; // must happen after direction is set
   processes[stpPin].first = HIGH;
   processes[stpPin].second = LOW;
-}
-
-void nothing() {
-  while(Serial.available()){
-      user_input = Serial.read(); //Read user input and trigger appropriate function
-      digitalWrite(EN, LOW); //Pull enable pin low to set FETs active and allow motor control
-      digitalWrite(ENY, LOW);
-      if (user_input =='1')
-      {
-         StepForwardDefault();
-      }
-      else if(user_input =='2')
-      {
-        DottedLineTest();
-      }
-      else if(user_input =='3')
-      {
-        SmallStepMode();
-      }
-      else if(user_input =='4')
-      {
-        ForwardBackwardStep();
-      }
-       else if(user_input =='5')
-      {
-        ReverseStepSlow();
-      }
-       else if(user_input =='6')
-      {
-        SmallStepReverseMode();
-      }
-      else if(user_input =='7')
-      {
-        CurrentTestMode();
-      }
-      else if(user_input =='8')
-      {
-        StepForwardDefaultY();
-      }
-       else if(user_input =='9')
-       {
-        SmallStepY();
-       }
-        else if(user_input =='0')
-       {
-        DashedLineTest();
-       }
-      else
-      {
-        Serial.println("Invalid option entered.");
-      }
-      resetBEDPins();
-  }
 }
 
 //Default microstep mode function
 void StepForwardDefault()
 {
   Serial.println("Moving forward at default step mode.");
-  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
+  stepBy(STP_E0, DIR_E0, 1000L, SECOND, 0L);
+  /* equivalent to:
+  digitalWrite(DIR_E0, LOW); //Pull direction pin low to move "forward"
   for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
   {
-    digitalWrite(stp,HIGH); //Trigger one step forward
+    digitalWrite(STP_E0,HIGH); //Trigger one step forward
     delay(1);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_E0,LOW); //Pull step pin low so it can be triggered again
     delay(1);
   }
-  Serial.println("Enter new option");
-  Serial.println();
+  */
 }
 
 void StepForwardDefaultY()
 {
   Serial.println("Moving Y forward at default step mode.");
-  digitalWrite(dirY, LOW); //Pull direction pin low to move "forward"
+  stepBy(STP_Y, DIR_Y, 200L, 2L * SECOND, 0L);
+  /* equivalent to:
+  digitalWrite(DIR_Y, LOW); //Pull direction pin low to move "forward"
   for(x= 1; x<200; x++)  //Loop the forward stepping enough times for motion to be visible
   {
-    digitalWrite(stpY,HIGH); //Trigger one step forward
+    digitalWrite(STP_Y,HIGH); //Trigger one step forward
     delay(2);
-    digitalWrite(stpY,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_Y,LOW); //Pull step pin low so it can be triggered again
     delay(2);
   }
-  Serial.println("Enter new option");
-  Serial.println();
+  */
 }
 
 //Reverse default microstep mode function
 void ReverseStepSlow()
 {
   Serial.println("Moving in reverse at slow step mode.");
-  digitalWrite(dir, HIGH); //Pull direction pin high to move in "reverse"
+  stepBy(STP_E0, DIR_E0, -200L, 5L * SECOND, 0L);
+  /* equivalent to:
+  digitalWrite(DIR_E0, HIGH); //Pull direction pin high to move in "reverse"
   for(x= 1; x<200; x++)  //Loop the stepping enough times for motion to be visible
   {
-    digitalWrite(stp,HIGH); //Trigger one step
+    digitalWrite(STP_E0,HIGH); //Trigger one step
     delay(5);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_E0,LOW); //Pull step pin low so it can be triggered again
     delay(5);
   }
-  Serial.println("Enter new option");
-  Serial.println();
+  */
 }
 
 //Reverse default microstep mode function long then forward to cut strand
@@ -390,21 +454,19 @@ void CurrentTestMode()
   digitalWrite(MS1, HIGH); //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
   digitalWrite(MS2, HIGH);
   digitalWrite(MS3, HIGH);
-  digitalWrite(dirY, LOW); //Pull direction pin low to move "forward"
-  digitalWrite(MS1Y, HIGH); //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
-  digitalWrite(MS2Y, HIGH);
-  digitalWrite(MS3Y, HIGH);
+  digitalWrite(DIR_Y, LOW); //Pull direction pin low to move "forward"
+  digitalWrite(MS1_Y, HIGH); //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
+  digitalWrite(MS2_Y, HIGH);
+  digitalWrite(MS3_Y, HIGH);
   for(x= 1; x<2000; x++)  //Loop the forward stepping enough times for motion to be visible
   {
-    digitalWrite(stp,HIGH); //Trigger one step forward
-    digitalWrite(stpY,HIGH);
+    digitalWrite(STP_E0,HIGH); //Trigger one step forward
+    digitalWrite(STP_Y,HIGH);
     delay(1);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-    digitalWrite(stpY,LOW);
+    digitalWrite(STP_E0,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_Y,LOW);
     delay(1);
   }
-  Serial.println("Enter new option");
-  Serial.println();
 }
 
 void Danger()
@@ -413,13 +475,11 @@ void Danger()
   digitalWrite(dir, HIGH); //Pull direction pin high to move in "reverse"
   for(x= 1; x<200; x++)  //Loop the stepping enough times for motion to be visible
   {
-    digitalWrite(stp,HIGH); //Trigger one step
+    digitalWrite(STP_E0,HIGH); //Trigger one step
     delayMicroseconds(500);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_E0,LOW); //Pull step pin low so it can be triggered again
     delayMicroseconds(500);
   }
-  Serial.println("Enter new option");
-  Serial.println();
 }
 
 void ReverseStepDefault()
@@ -428,13 +488,11 @@ void ReverseStepDefault()
   digitalWrite(dir, HIGH); //Pull direction pin high to move in "reverse"
   for(x= 1; x<1000; x++)  //Loop the stepping enough times for motion to be visible
   {
-    digitalWrite(stp,HIGH); //Trigger one step
+    digitalWrite(STP_E0,HIGH); //Trigger one step
     delay(1);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_E0,LOW); //Pull step pin low so it can be triggered again
     delay(1);
   }
-  Serial.println("Enter new option");
-  Serial.println();
 }
 
 // 1/16th microstep foward mode function
@@ -447,32 +505,28 @@ void SmallStepMode()
   digitalWrite(MS3, HIGH);
   for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
   {
-    digitalWrite(stp,HIGH); //Trigger one step forward
+    digitalWrite(STP_E0,HIGH); //Trigger one step forward
     delay(1);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_E0,LOW); //Pull step pin low so it can be triggered again
     delay(1);
   }
-  Serial.println("Enter new option");
-  Serial.println();
 }
 
 // 1/16th microstep foward mode function
 void SmallStepY()
 {
   Serial.println("Stepping at 1/16th microstep mode.");
-  digitalWrite(dirY, LOW); //Pull direction pin low to move "forward"
-  digitalWrite(MS1Y, HIGH); //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
-  digitalWrite(MS2Y, HIGH);
-  digitalWrite(MS3Y, HIGH);
+  digitalWrite(DIR_Y, LOW); //Pull direction pin low to move "forward"
+  digitalWrite(MS1_Y, HIGH); //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
+  digitalWrite(MS2_Y, HIGH);
+  digitalWrite(MS3_Y, HIGH);
   for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
   {
-    digitalWrite(stpY,HIGH); //Trigger one step forward
+    digitalWrite(STP_Y,HIGH); //Trigger one step forward
     delay(1);
-    digitalWrite(stpY,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_Y,LOW); //Pull step pin low so it can be triggered again
     delay(1);
   }
-  Serial.println("Enter new option");
-  Serial.println();
 }
 
 // 1/16th microstep foward mode function
@@ -485,13 +539,11 @@ void SmallStepReverseMode()
   digitalWrite(MS3, HIGH);
   for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
   {
-    digitalWrite(stp,HIGH); //Trigger one step forward
+    digitalWrite(STP_E0,HIGH); //Trigger one step forward
     delay(1);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(STP_E0,LOW); //Pull step pin low so it can be triggered again
     delay(1);
   }
-  Serial.println("Enter new option");
-  Serial.println();
 }
 
 //Forward/reverse stepping function
@@ -513,14 +565,12 @@ void ForwardBackwardStep()
     
     for(y=1; y<1000; y++)
     {
-      digitalWrite(stp,HIGH); //Trigger one step
+      digitalWrite(STP_E0,HIGH); //Trigger one step
       delay(1);
-      digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+      digitalWrite(STP_E0,LOW); //Pull step pin low so it can be triggered again
       delay(1);
     }
   }
-  Serial.println("Enter new option");
-  Serial.println();
 }
 
 void DashedLineTest()
@@ -543,8 +593,6 @@ void DashedLineTest()
   SmallStepMode();
   SmallStepMode();
   SmallStepY();
-  Serial.println("Enter new option");
-  Serial.println();
 }
 
 void DottedLineTest()
@@ -568,6 +616,4 @@ void DottedLineTest()
   SmallStepMode();
   SmallStepMode();
   SmallStepY();
-  Serial.println("Enter new option");
-  Serial.println();
 }
